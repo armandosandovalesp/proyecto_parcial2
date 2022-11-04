5ec082fc39ff073261ed09ba33a7b377562bc9ad
EduardoGrijalva18 <99852224+EduardoGrijalva18@users.noreply.github.com>
2022-11-03 19:21:16 -0700

    Add files via upload

---
+++ proyecto_parcial2/PracticaWebDatos/eliminarRegistro.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro Eliminado</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+</body>
+</html>
+
+
+<?php
+    
+    include 'conexion.php';
+
+    $id = $_GET["id"];
+
+    $sql = "DELETE FROM juegos WHERE id=" . $id;
+
+    if($conexion->query($sql) === TRUE){
+        echo "Se eliminó el registro con ID " .$id. "<br>";
+        echo "<a href='consultarDatos.php'>Regresar</a>";
+    } else {
+        echo "Ocurrió un error: " . $conexion->error ."<br>";
+        echo "<a href='consultarDatos.php'>Regresar</a>";
+    }
+
+?>
